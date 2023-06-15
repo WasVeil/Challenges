72b8f4efc35a6dffcfe7dd3ed9c0d4740517a585
Aviva <123884383+zAviva@users.noreply.github.com>
2023-06-15 11:27:51 -0500

    Create b-a.lua

---
+++ Original/b-a.lua
@@ -0,0 +1,12 @@
+--[[
+  CAN'T HOOK PRINT.
+  CAN'T CHANGE STRING METATABLE.
+  CAN'T CHANGE ANY GIVEN CODE.
+
+  Objective: Must print "b" before printing "a".
+]]
+
+--// solution above
+
+print("a")
+print("b")
