19145ed8f69016e6ac062d872f46c4c82d936935
Aviva <123884383+zAviva@users.noreply.github.com>
2023-06-14 19:18:21 -0500

    Update and rename base.txt to 1+1.lua

---
+++ Original/1+1.lua
@@ -0,0 +1,13 @@
+--[[
+  RULES:
+  You CAN'T hook print.
+  You CAN'T edit anything below "--// SOLUTION ABOVE"
+  IT MUST OUTPUT 3.
+]]
+
+--// SOLUTION BELOW
+
+
+--// SOLUTION ABOVE
+
+print(1+1)
